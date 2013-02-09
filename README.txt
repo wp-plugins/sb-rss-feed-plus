7e09d7bbba5d5ea9702e3f51dae3837b543bc1bd
ladislav.soukup@gmail.com <ladislav.soukup@gmail.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-09 23:07:20 +0000

    - WordPress excerpt_only setting can be "temporary" overridden via special url (query param)
    git-svn-id: https://plugins.svn.wordpress.org/sb-rss-feed-plus/tags/1.3@665740 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.txt
@@ -0,0 +1,96 @@
+=== SB RSS feed plus ===
+Contributors: ladislav.soukup@gmail.com
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=P6CKTGSXPFWKG&lc=CZ&item_name=Ladislav%20Soukup&item_number=SB%20RSS%20feed%20plus%20%5bWP%2dPlugin%5d&currency_code=EUR&bn=PP%2dDonationsBF%3abtn_donate_SM%2egif%3aNonHosted
+Tags: rss, feed, image, post thumbnail, add, enhance, enhanced, plus, better, flipboard, google currents, content:encoded, media:content, media, content, ad, ads, advertisement, override
+Requires at least: 3.3.1
+Tested up to: 3.5
+Stable tag: 1.3
+
+This plugin will add post thumbnail to RSS feed items.
+
+== Description ==
+
+You can improve the default WordPress RSS feed to include:
+
+- This plugin will add post thumbnail to RSS feed as "media:content" and "enclosure" tags.
+- Image is also added to HTML part of "description" and "content:encoded" tags.
+- You can also add server signature to end of feed content in form "Source: XYZ".
+- Inject advertisement (image with link) after nth paragraph of each post.
+
+= Post thumbnail =
+Add post thumnail to each post's excerpt and full text (if enabled in WordPress configuration).
+Image will be added just before text of each post in RSS feed.
+Post thumbnail is also added as media:content and enclosure tag to RSS feed.
+
+= Full-text RSS Feed when accessed via special URL =
+You can access RSS Feed with full content posts even when WordPress is set to RSS "Excerpt only" mode. Just setup a "secret key" and enable the override...
+You can then request feed url with special query param ?fsk= to override "excerpt only" mode to "fulltext" mode. This is great for services like Google Currents.
+
+= Server signature =
+You can add server signature just after the full text content of post to each RSS post.
+
+= ADs =
+Very simple implementation of advertisement to each RSS feed post item. Ad is a simple clickable image (stretched to 90% of width - maximum of 700px).
+There is no click monitoring, so you should use something like bit.ly to track clicks.
+
+= Translatable =
+All text can be translated using standart language files, text domain is: "SB_RSS_feed_plus".
+
+Included translations:
+- English (default)
+- Czech
+
+== Installation ==
+
+1. Upload to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Feed is now updated, you should check it
+
+No more settings are needed.
+
+== Screenshots ==
+
+1. Plugin settings
+
+== Changelog ==
+
+= 1.3 =
+- WordPress excerpt_only setting can be "temporary" overridden via special url (query param)
+
+= 1.2 =
+
+- added advertisement injection
+- few core updates
+- bug fixes
+
+= 1.1.2 =
+
+- admin notification code updated (code clean up)
+
+
+= 1.1 =
+
+- settings page updated
+- settings moved under "Settings" section of WordPress
+- default settings fixed
+- warning notification if settings are out of date (after plugin update)
+- settings updated - you need to update configurtion in admin
+
+= 1.0 =
+
+- added settings
+- few fixes, code clanup
+- Czech translation updated
+
+= 0.2 =
+
+- fixed <content:encoded> - now it extends WordPress default tag
+- fixed repeated excerpt problem
+- full text of post is only embeded if this option is allowed in WordPress (native RSS settings)
+- "Source: XYZ" added to end of content (link back to your site)
+- added Czech translation, plugin is multi-language ready
+
+= 0.1 =
+
+first beta version
+
---
+++ settings/sbrssfeed-cfg.php
@@ -0,0 +1,153 @@
+<?php
+global $wpsf_settings;
+$CFG = wpsf_get_settings( $this->plugin_path .'settings/sbrssfeed-cfg.php' );
+
+$wpsf_settings[] = array(
+    'section_id' => 'tags',
+    'section_title' => __( 'Add Image RSS Feed tags', 'SB_RSS_feed_plus' ),
+    'section_description' => __( '"enclosure" and "media:content" tag in RSS feed are used to tell RSS parser about post thumbnail.', 'SB_RSS_feed_plus' ),
+    'section_order' => 10,
+    'fields' => array(
+		array(
+            'id' => 'addTag_enclosure',
+            'title' => __( 'Add "enclosure" tag to RSS feed', 'SB_RSS_feed_plus' ),
+            'desc' => '',
+            'type' => 'checkbox',
+            'std' => 1
+        ),
+		array(
+            'id' => 'addTag_mediaContent',
+            'title' => __( 'Add "media:content" tag to RSS feed', 'SB_RSS_feed_plus' ),
+            'desc' => '',
+            'type' => 'checkbox',
+            'std' => 1
+        )
+	)
+);
+
+$wpsf_settings[] = array(
+    'section_id' => 'description',
+    'section_title' => __( 'Extend HTML content', 'SB_RSS_feed_plus' ),
+    'section_description' => __( 'This will extend the HTML code of "description" and "content:encoded" tags with 90% wide image before the text.', 'SB_RSS_feed_plus' ),
+    'section_order' => 20,
+    'fields' => array(
+		array(
+            'id' => 'extend_description',
+            'title' => __( 'Extend "description" (excerpt)', 'SB_RSS_feed_plus' ),
+            'desc' => '',
+            'type' => 'checkbox',
+            'std' => 1
+        ),
+		array(
+            'id' => 'extend_content',
+            'title' => __( 'Extend "content:encoded" HTML', 'SB_RSS_feed_plus' ),
+            'desc' => '',
+            'type' => 'checkbox',
+            'std' => 1
+        )
+	)
+);
+
+$rss_use_excerpt = get_option('rss_use_excerpt');
+$rss_fulltext_link = site_url() . '/feed/?fsk=';
+$CFG['sbrssfeedcfg_fulltext_fulltext_override_secrete'] ? $rss_fulltext_link .= $CFG['sbrssfeedcfg_fulltext_fulltext_override_secrete'] : $rss_fulltext_link .= '-NOT-SET-';
+
+$wpsf_settings[] = array(
+    'section_id' => 'fulltext',
+    'section_title' => __( 'RSS Feed fulltext override', 'SB_RSS_feed_plus' ),
+    'section_description' => __( 'Override "excerpt only" RSS feed when requested with "secret" key.', 'SB_RSS_feed_plus' ),
+    'section_order' => 25,
+    'fields' => array(
+		array(
+            'id' => 'fulltext_wp_option',
+            'title' => __( 'WordPress RSS Feed mode', 'SB_RSS_feed_plus' ),
+            'desc' => '',
+			'type' => 'custom',
+			'std' => $rss_use_excerpt ? __( 'Excerpt only - there is only excerpt in the standard RSS Feed...<br />However, requesting feed url with special "secret key" will display full content of each post (great for services like Google Currents).', 'SB_RSS_feed_plus' ) : __( 'Fulltext - your feed already contains whole post content.', 'SB_RSS_feed_plus' )
+        ),
+		array(
+            'id' => 'fulltext_override',
+            'title' => __( 'Enable fulltext override', 'SB_RSS_feed_plus' ),
+            'desc' => $rss_use_excerpt ? '<em>' . __( 'When enabled, you can request RSS Feed with full post content with special URL (added query string <strong>?fsk=</strong>)', 'SB_RSS_feed_plus' ) . '</em>' : '<em>' . __( 'You don\'t need to override WordPress settings - your feed already contains full post content.', SB_RSS_feed_plus ) . '</em>' ,
+            'type' => 'checkbox',
+            'std' => 0
+        ),
+		array(
+            'id' => 'fulltext_override_secrete',
+            'title' => __( 'Override "secret" key (?fsk= param)', 'SB_RSS_feed_plus' ),
+            'desc' => __( 'Fulltext RSS Feed:', 'SB_RSS_feed_plus' ) . ' <a href="'.$rss_fulltext_link.'" target="_blank">' . $rss_fulltext_link . '</a>',
+            'type' => 'text',
+			'std' => uniqid()
+        )
+	)
+);
+
+$wpsf_settings[] = array(
+    'section_id' => 'signature',
+    'section_title' => __( 'RSS Feed signature', 'SB_RSS_feed_plus' ),
+    'section_description' => __( 'Add "Source: XYZ" text to end of the content of each feed item.', 'SB_RSS_feed_plus' ),
+    'section_order' => 30,
+    'fields' => array(
+		array(
+            'id' => 'addSignature',
+            'title' => __( 'Add signature', 'SB_RSS_feed_plus' ),
+            'desc' => '',
+            'type' => 'checkbox',
+            'std' => 0
+        )
+	)
+);
+
+$wpsf_settings[] = array(
+    'section_id' => 'inrssAd',
+    'section_title' => __( 'RSS Feed advertisement', 'SB_RSS_feed_plus' ),
+	'section_description' => __( 'Inject "ad" to RSS feed items (image with link). Ad will be inserted only to full text "content:encoded" tag.', 'SB_RSS_feed_plus' ),
+    'section_order' => 40,
+    'fields' => array(
+		array(
+            'id' => 'inrssAd_enabled',
+            'title' => __( 'Inject ad to feed posts', 'SB_RSS_feed_plus' ),
+            'desc' => '',
+            'type' => 'checkbox',
+            'std' => 0
+        ),
+		array(
+			'id' => 'inrssAd_img',
+			'title' => __( 'Ad image', 'SB_RSS_feed_plus' ),
+			'desc' => __( 'image will be stretched up to 700px of width', 'SB_RSS_feed_plus' ),
+			'type' => 'file',
+			'std' => ''
+        ),
+		array(
+            'id' => 'inrssAd_title',
+            'title' => __( 'Ad title', 'SB_RSS_feed_plus' ),
+            // 'desc' => __( 'Will be inserted as &lt;figure&gt; tag and alt attribute of img tag', 'SB_RSS_feed_plus' ),
+            'type' => 'text'
+        ),
+		array(
+            'id' => 'inrssAd_link',
+            'title' => __( 'Ad target link', 'SB_RSS_feed_plus' ),
+            'desc' => __( 'Recommendation: use bit.ly or similar service to track clicks...', 'SB_RSS_feed_plus' ),
+            'type' => 'text'
+        ),
+		array(
+            'id' => 'inrssAd_injectAfter',
+            'title' => __( 'Inject ad after nth paragraph', 'SB_RSS_feed_plus' ),
+            'type' => 'select',
+			'type' => 'select',
+            'std' => '2',
+			'choices' => array(
+				'1' => __( '1st paragraph', 'SB_RSS_feed_plus' ),
+				'2' => __( '2nd paragraph', 'SB_RSS_feed_plus' ),
+				'3' => __( '3rd paragraph', 'SB_RSS_feed_plus' ),
+				'4' => __( '4th paragraph', 'SB_RSS_feed_plus' ),
+				'5' => __( '5th paragraph', 'SB_RSS_feed_plus' ),
+				'6' => __( '6th paragraph', 'SB_RSS_feed_plus' ),
+				'7' => __( '7th paragraph', 'SB_RSS_feed_plus' ),
+			)
+        )
+	)
+);
+
+
+?>
